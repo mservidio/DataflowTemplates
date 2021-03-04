6148267da4a36ebf2cd338c629b723bdc51c2c46
Mark Servidio <mservidio@google.com>
2021-03-04 14:47:27 -0500

    Create FixedWidthColumn.java

---
+++ v2/fixed-width-loader/src/main/java/com/google/cloud/teleport/v2/templates/FixedWidthColumn.java
@@ -0,0 +1,55 @@
+public class FixedWidthColumn {
+  /** The {@link DataTypes} enum contains all valid datatypes. */
+  public enum DataType {
+    DATE,
+    NUMERIC,
+    STRING
+  }
+
+  private int offset;
+  private int length;
+  private String fieldName;
+  private DataType type;
+  private String format;
+
+  public int getOffset() {
+    return offset;
+  }
+
+  public void setOffset(int offset) {
+    this.offset = offset;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public void setLength(int length) {
+    this.length = length;
+  }
+
+  public String getFieldName() {
+    return fieldName;
+  }
+
+  public void setFieldName(String fieldName) {
+    this.fieldName = fieldName;
+  }
+
+  public DataType getType() {
+    return type;
+  }
+
+  public void setType(DataType type) {
+    this.type = type;
+  }
+
+  public String getFormat() {
+    return format;
+  }
+
+  public void setFormat(String format) {
+    this.format = format;
+  }
+}
+
